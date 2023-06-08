e3969c961cff83e520cfc1c6f99256ec9aae4cb8
Alan Marko <alan.marko@trili.tech>
2023-06-08 09:59:21 +0100

    Brackets removed

--- src/components/ChatBob.jsx
+++ src/components/ChatBob.jsx
@@ -25,7 +25,6 @@ export function ChatBob() {
       >
         <ChatIcon/>
       </Fab>
-      (
         <div style={{ borderRadius: '100px', zIndex: 20}}>
         <iframe
           src="https://tezosbot.vercel.app/chatbox?history_enabled=true"
@@ -45,7 +44,6 @@ export function ChatBob() {
           }}
         />
         </div>
-      )
     </>
   );
 };
