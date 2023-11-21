de78b511c105c07cf5b969e8f350101c976b696a
Tim McMackin <timothymcmackin@yahoo.com>
2023-11-21 13:29:06 -0500

    First draft of Unity quickstart

--- sidebars.js
+++ sidebars.js
@@ -187,6 +187,17 @@ const sidebars = {
         'dApps/wallets',
         'dApps/sending-transactions',
         'dApps/taquito',
+        {
+          type: 'category',
+          label: 'Tezos SDK for Unity',
+          // link: { // TODO new overview
+          //   id: 'dApps/unity',
+          //   type: 'doc',
+          // },
+          items: [
+            'dApps/unity/quickstart',
+          ],
+        },
         // 'dApps/frameworks', // TODO
         // Hide defi for now because the content is very outdated
         // 'dApps/defi',
