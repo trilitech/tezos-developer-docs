5d9bc649f93cc4e002841c7228d99df82a22bc22
Tim McMackin <timothymcmackin@users.noreply.github.com>
2024-01-05 13:16:17 -0500

    put GTM first so it gets loaded regardless of the ; problem (#249)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,15 +3,19 @@
 
 const math = require('remark-math');
 const katex = require('rehype-katex');
-const fs = require('fs').promises;
 
-const metaTagStringToReplace = 'contentOfContentSecurityPolicyGoesHere';
+// script-src causes development builds to fail
+// But unsafe-eval should NOT be in production builds
+// Also, put GTM first because sometimes the ';' in the escaped single quotes causes the browser to think it's the end
+const scriptSrc = process.env.NODE_ENV === 'development' ?
+  `https://*.googletagmanager.com 'self' 'unsafe-inline' 'unsafe-eval'`
+  : `https://*.googletagmanager.com 'self' 'unsafe-inline'`;
 
 const contentSecurityPolicy = `
 default-src 'none';
 base-uri 'self';
 manifest-src 'self';
-script-src 'self' 'unsafe-inline' https://*.googletagmanager.com;
+script-src ${scriptSrc};
 style-src 'self' 'unsafe-inline';
 font-src 'self';
 img-src 'self' https://*.googletagmanager.com https://*.google-analytics.com data:;
@@ -19,25 +23,6 @@ media-src 'self';
 form-action 'self';
 connect-src 'self' https://*.algolia.net https://*.algolianet.com https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com;
 frame-src https://tezosbot.vercel.app https://calendly.com/ lucid.app;`;
-
-// Update the CSP tsg after builds
-// because docusaurus always escapes the quotes
-// https://github.com/facebook/docusaurus/issues/9686
-const updateMetaTag = async (outDir, route) => {
-  const filePath = route.endsWith('.html')
-    ? outDir + route
-    : outDir + route + '/index.html';
-  const fileContent = await fs.readFile(filePath,
-  'utf8');
-  const updatedFileContent = fileContent.replace(metaTagStringToReplace, contentSecurityPolicy);
-  await fs.writeFile(filePath, updatedFileContent, 'utf8');
-}
-
-// script-src causes development builds to fail
-// But unsafe-eval should NOT be in production builds
-const scriptSrc = process.env.NODE_ENV === 'development' ?
-  `'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com;`
-  : `'self' 'unsafe-inline' https://*.googletagmanager.com;`;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -63,7 +48,7 @@ const config = {
       tagName: 'meta',
       attributes: {
         'http-equiv': 'Content-Security-Policy',
-        content: metaTagStringToReplace,
+        content: contentSecurityPolicy,
       },
     },
   ],
@@ -95,13 +80,6 @@ const config = {
 
   plugins: [
     'plugin-image-zoom',
-    () => ({
-      async postBuild({ routesPaths, outDir }) {
-        await Promise.all(routesPaths.map((oneRoute) =>
-          updateMetaTag(outDir, oneRoute)
-        ));
-      },
-    }),
   ],
 
   themeConfig:
