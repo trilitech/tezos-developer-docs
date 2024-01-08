468623a301231b680a36b47ac4fcd4a92681c84a
Tim McMackin <timothymcmackin@yahoo.com>
2024-01-08 11:31:37 -0500

    Allow passing a list of files to test

--- test/images.test.mjs
+++ test/images.test.mjs
@@ -12,21 +12,21 @@ import { fromMarkdown } from 'mdast-util-from-markdown';
 import { mdxFromMarkdown } from 'mdast-util-mdx'
 import { mdxjs } from 'micromark-extension-mdxjs'
 import { visit } from 'unist-util-visit';
-import minimist from 'minimist';
-
-const argv = minimist(process.argv.slice(2));
 
 import { exampleAstWithBrokenLinks, expectedImagesInAst } from './resources/imageCheckTestResources.mjs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const filesToTestPath = path.resolve(__dirname, '../filesToTest.txt');
 
 const docsFolder = path.resolve(__dirname, '../docs');
 const imageFolder = path.resolve(__dirname, '../static');
 
 // Get file names passed in the command or if none were passed, use all files
-const getFilePaths = () => {
-  if (argv._.length > 0) {
-    return argv._.map((filePath) => path.resolve(__dirname, '../', filePath));
+const getFilePaths = async () => {
+  if (fs.existsSync(filesToTestPath)) {
+    const fileData = await fs.promises.readFile(filesToTestPath, 'utf8');
+    const filesToTest = fileData.split('\n');
+    return filesToTest;
   } else {
     return glob(docsFolder + '/**/*.{md,mdx}');
   }
@@ -40,6 +40,67 @@ const processor = unified()
   .use(remarkParse)
   .use(remarkRehype)
   .use(rehypeStringify);
+
+  // Test functions to select nodes that are links to images
+  const mdxTestFunction = (node) => ['img', 'Figure'].includes(node.name);
+  const markdownTestFunction = (node) => node.type === 'image';
+
+  // Get all of the images in an AST, visiting the correct nodes for MD and MDX files.
+  /*
+  For MDX, get Figure and img nodes, like this img node:
+  {
+    "type": "mdxJsxFlowElement",
+    "name": "img",
+    "attributes": [
+      {
+        "type": "mdxJsxAttribute",
+        "name": "src",
+        "value": "/img/tezos_smart_contract_content.svg"
+      },
+      {
+        "type": "mdxJsxAttribute",
+        "name": "alt",
+        "value": "hi"
+      }
+    ],
+    "children": [],
+    "position": {...}
+  },
+
+  For MD, get image nodes, like this:
+  {
+    "type": "image",
+    "title": null,
+    "url": "/img/someimage.jpg",
+    "alt": "some alt text",
+    "position": {...}
+  },
+  */
+  const getImagesInAst = (ast, /*filePath*/) => {
+    const imagePathsInFile = [];
+    // MDX elements
+    visit(ast, mdxTestFunction, (node) => {
+      const srcAttribute = node.attributes.find((attr => attr.name === 'src'));
+      imagePathsInFile.push(srcAttribute.value);
+    });
+    // MD images
+    visit(ast, markdownTestFunction, (node) => {
+      imagePathsInFile.push(node.url);
+    });
+    // Filter out external links to files
+    return imagePathsInFile.filter((oneLink) =>
+      !oneLink.startsWith('http://') && !oneLink.startsWith('https://')
+    );
+  }
+
+  test('Verify that the test gets images from ASTs', () => {
+    const imagesFoundInAst = getImagesInAst(exampleAstWithBrokenLinks);
+    expectedImagesInAst.forEach(oneExpectedImage => {
+      expect(imagesFoundInAst,
+        'Image check test failed. getImagesInAst did not find an image it should have:' + oneExpectedImage)
+        .toContain(oneExpectedImage);
+    });
+  });
 
 test.each(await filePathsPromise)('Check image paths in %s', async (filePath) => {
   const availableImagePaths = await getImagePaths();
@@ -66,64 +127,3 @@ const getAst = async (filePath) => {
     return processor.parse(fileContents);
   }
 }
-
-// Test functions to select nodes that are links to images
-const mdxTestFunction = (node) => ['img', 'Figure'].includes(node.name);
-const markdownTestFunction = (node) => node.type === 'image';
-
-// Get all of the images in an AST, visiting the correct nodes for MD and MDX files.
-/*
-For MDX, get Figure and img nodes, like this img node:
-{
-  "type": "mdxJsxFlowElement",
-  "name": "img",
-  "attributes": [
-    {
-      "type": "mdxJsxAttribute",
-      "name": "src",
-      "value": "/img/tezos_smart_contract_content.svg"
-    },
-    {
-      "type": "mdxJsxAttribute",
-      "name": "alt",
-      "value": "hi"
-    }
-  ],
-  "children": [],
-  "position": {...}
-},
-
-For MD, get image nodes, like this:
-{
-  "type": "image",
-  "title": null,
-  "url": "/img/someimage.jpg",
-  "alt": "some alt text",
-  "position": {...}
-},
-*/
-const getImagesInAst = (ast, /*filePath*/) => {
-  const imagePathsInFile = [];
-  // MDX elements
-  visit(ast, mdxTestFunction, (node) => {
-    const srcAttribute = node.attributes.find((attr => attr.name === 'src'));
-    imagePathsInFile.push(srcAttribute.value);
-  });
-  // MD images
-  visit(ast, markdownTestFunction, (node) => {
-    imagePathsInFile.push(node.url);
-  });
-  // Filter out external links to files
-  return imagePathsInFile.filter((oneLink) =>
-    !oneLink.startsWith('http://') && !oneLink.startsWith('https://')
-  );
-}
-
-test('Verify that the test gets images from ASTs', () => {
-  const imagesFoundInAst = getImagesInAst(exampleAstWithBrokenLinks);
-  expectedImagesInAst.forEach(oneExpectedImage => {
-    expect(imagesFoundInAst,
-      'Image check test failed. getImagesInAst did not find an image it should have:' + oneExpectedImage)
-      .toContain(oneExpectedImage);
-  });
-});
