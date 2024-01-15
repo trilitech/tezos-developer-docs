05340ad60e40aa2bd7a9e429bb910202a4b98738
Tim McMackin <timothymcmackin@yahoo.com>
2024-01-15 09:14:58 -0500

    Revert "Test credentials from algolia"
    This reverts commit a77cafb5c3df145a502e6f80350a0f230d93f26a.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,10 +137,10 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID || "QPBQ67WNIG",
+        appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID || "QRIAHGML9Q",
         // Public API key: it is safe to commit it
-        apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY || "cd621124ed2d83249bf003f4a69f391c",
-        indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME || "crawler_tezos-test",
+        apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY || "57d6a376a3528866784a143809cc7427",
+        indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME || "tezosdocs",
         // Optional: see doc section below
         contextualSearch: true,
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
