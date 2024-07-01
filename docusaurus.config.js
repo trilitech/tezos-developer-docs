001688cdbf1a4f122c94a2e93fb075d1653172c5
Tim McMackin <timothymcmackin@users.noreply.github.com>
2024-07-01 14:25:32 -0400

    got this backwards (#419)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
   markdown: {
     mermaid: true,
   },
-  noIndex: process.env.BUILD_ENV == 'staging' ? false : true,
+  noIndex: process.env.BUILD_ENV == 'staging' ? true : false,
 
   headTags: [
     {
